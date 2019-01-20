08258a2f42f30e380488bc8e9e51043805342e90
Foysol Ahmed Shuvo <43276773+root120@users.noreply.github.com>
2019-01-20 22:58:32 +0600

    0(n^2) solution

--- Container With Most Water.cpp
+++ Container With Most Water.cpp
@@ -6,9 +6,9 @@
 Problem Description: Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
 Input: [1,8,6,2,5,4,8,3,7]
 Output: 49
-Explanation: consider 2 and last number from given array.
+Explanation: consider index number 2 and last index from given array.
 
-Solution Idea: First of all you take each index from you'r string, than just check after the each index you get the maximum length of string. the string carry only distinct character.
+Solution Idea: Just You take a value form given array. than check each elemnet calculate the current value. than max collect max from all value. 
 Complexity: O(n^2)
 
 Note: I have a O(n) solution for this problem. (using stack)
